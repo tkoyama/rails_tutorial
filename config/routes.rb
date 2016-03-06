1093d46fd2eb5cd792e93c6a0821c97fa4b1bee9
vividmuimui <tkoyama@aiming-inc.com>
2016-03-07 03:09:59 +0900

    12章の終わり

--- config/routes.rb
+++ config/routes.rb
@@ -18,8 +18,15 @@ resources :users
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 end
